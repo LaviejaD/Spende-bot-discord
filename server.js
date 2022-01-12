c2d40053f935b09239131aecd9e01031013f2941
laviejaD <dmmmgaymer@gmail.com>
2022-01-11 19:56:51 -0500

    numb cambiado a parseInt

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const uptime = new Date(Numb(process.env['TIMEUP']))
+const uptime = new Date(parseInt(process.env['TIMEUP']))
 
 require('http').createServer((req, res) => res.end(`Days:${uptime.getDay},`)).listen(3000)
 require('./dist/index.js')
