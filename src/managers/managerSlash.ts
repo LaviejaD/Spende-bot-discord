d0c1f9e43a0c67ac05b03f05653581c71e779246
laviejaD <dmmmgaymer@gmail.com>
2022-06-12 19:45:04 -0500

    optimizando slash comands

--- src/managers/managerSlash.ts
+++ src/managers/managerSlash.ts
@@ -6,47 +6,41 @@ import { Routes } from "discord-api-types/v9";
 
 import { log } from "../utils/log";
 import { configbot } from "../configuration/bot";
-const { prefix, GuildId } = configbot;
+const { GuildId } = configbot;
 
 interface slashcommandIterface {
 	name: string, data: any, execute(client: Client, interaction: Interaction): void
 }
 
-const CommandSlash = new Map<string, (client: Client, interaction: Interaction) => void>()
+const CommandSlash = new Map<string, string>()
 
 export function SlashRegister() {
 	return new Promise<void>(async (done) => {
 		log(`Register Slash Commands`)
-
 		let Commands: any[] = []
 		const rest = new REST({ version: '9' }).setToken(`${process.env['BOT_TOKEN']}`)
 		const folders = readdirSync(join(__dirname, '../commandsSlash'))
 
 		folders
 			.forEach((folder) => {
-
 				const pathFile = join(__dirname, '../commandsSlash', `${folder}`)
-				const { name, data, execute }: slashcommandIterface = require(join(pathFile, `./index`))
+				const { name, data }: slashcommandIterface = require(join(pathFile, `./index`))
 
 				Commands.push(data.toJSON())
-				CommandSlash.set(name, execute)
+				CommandSlash.set(name, join(pathFile, `./index`))
 				log(`Commands slash ${folder} is save`, 'loader')
 			})
 
 		const route = process.env['DEV'] == "true"
 			? Routes.applicationGuildCommands(`${process.env['BOTCLIENT']}`, GuildId)
 			: Routes.applicationCommands(`${process.env['BOTCLIENT']}`);
-
 		try { await rest.put(route, { body: Commands }) }
 		catch (error) { console.log(error.name) }
-
-
 		done()
-
 	})
 }
 
 export function ManagarSlash(commandName: string, interaction: Interaction, client: Client): void {
-	const execute = CommandSlash.get(commandName)
+	const execute: (client: Client, interaction: Interaction) => void = require(CommandSlash.get(commandName)).execute
 	if (execute) return execute(client, interaction)
 }
